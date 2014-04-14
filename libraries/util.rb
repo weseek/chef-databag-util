bdb7dc835d9ac1fdf6d28b947b19bb46a751968f
Yuki Takei <yuki@weseek.co.jp>
2014-04-14 12:45:09 +0900

    BugFix

--- libraries/util.rb
+++ libraries/util.rb
@@ -2,7 +2,7 @@ class Chef::Recipe::DatabagUtil
   
   def self.get_data_bag_item_safely(key, id)
     return begin
-      data_bag_item(key, id)
+      Chef::Recipe::data_bag_item(key, id)
     # if can't get data_bag_item, returns {} (default value is an empty string)
     rescue Net::HTTPServerException
       Hash.new("")
