5fe52da621eb573976fcb5544c15ff4a8422ea7c
Yuki Takei <yuki@weseek.co.jp>
2014-04-14 12:14:15 +0900

    add 'self.' (define as class method)

--- libraries/util.rb
+++ libraries/util.rb
@@ -1,6 +1,6 @@
 class Chef::Recipe::DatabagUtil
   
-  def get_data_bag_item_safely(key, id)
+  def self.get_data_bag_item_safely(key, id)
     return begin
       data_bag_item(key, id)
     # if can't get data_bag_item, returns {} (default value is an empty string)
